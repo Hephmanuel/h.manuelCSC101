b22d3dd90a8beef8cd5d1e5c7b32e4880618e3a3
Manuela Manuel <manuelamanuel326@gmail.com>
2022-11-19 17:30:01 +0100

    (My third commit)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let special_character = '@'; //defailt
+    let alphabet:char = 'B';
+    let surname = "Bolaji";
+    let first_name = "Michael";
+    let middle_name = "Gboyega";
+
+    println!(" ");
+    ptintln!("Special character: {}", special_character);
+    println!("Alphabet: {}");
+    println!("Name: {} {} {}", surname. middle_name, first_name);
+
+
+
+}
---
+++ week-3/practice_6/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+   let interest:f32 = 8; //integer attached to float variable
+   println!("interest is {}", interest);
+}
